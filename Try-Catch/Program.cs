63d226c6aaf7419edc71ae550af9e2cfbb551544
Ali Avşar <vimsagrup@VimsaGrup.onmicrosoft.com>
2024-11-06 14:00:07 +0300

    try

---
+++ Try-Catch/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Write("Bir sayı giriniz: ");
+            string kullaniciGirdisi = Console.ReadLine();
+
+            try
+            {
+                int sayi = int.Parse(kullaniciGirdisi);
+                int karesi = sayi * sayi;
+                Console.WriteLine($"{sayi} sayısının karesi: {karesi}");
+                break; 
+            catch (FormatException)
+            {
+                Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+            }
+        }
+    }
+}
