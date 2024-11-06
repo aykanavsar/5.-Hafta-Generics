8e8a6f5abf3f7555fd7715b7a0768dca21cc6bdc
Ali Avşar <vimsagrup@VimsaGrup.onmicrosoft.com>
2024-11-06 13:39:19 +0300

    list 2 ödev

---
+++ 5. Hafta List - 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Kahve isimlerini depolamak için bir liste oluşturuyoruz
+        List<string> kahveIsimleri = new List<string>();
+
+        // Kullanıcıdan 5 adet kahve ismi alıyoruz
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write($"Lütfen {i}. kahve ismini girin: ");
+            string kahveIsmi = Console.ReadLine();
+            kahveIsimleri.Add(kahveIsmi);
+        }
+
+        // Girilen kahve isimlerini ekrana yazdırıyoruz
+        Console.WriteLine("\nGirilen Kahve İsimleri:");
+        foreach (string kahve in kahveIsimleri)
+        {
+            Console.WriteLine($"- {kahve}");
+        }
+    }
+}
