6f512fedf5fbd28c73b60f9fec0652bb4c53a2bf
Ali Avşar <vimsagrup@VimsaGrup.onmicrosoft.com>
2024-11-06 12:50:26 +0300

    list ödev

---
+++ 5. Hafta List/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Ziyaretçilerin isimlerini içeren bir liste tanımlayın
+        List<string> ziyaretciler = new List<string>()
+        {
+            "Bülent Ersoy",
+            "Ajda Pekkan",
+            "Ebru Gündeş",
+            "Hadise",
+            "Hande Yener",
+            "Tarkan",
+            "Funda Arar",
+            "Demet Akalın"
+        };
+
+        // İsimleri tek tek ekrana yazdır
+        Console.WriteLine("Ziyaretçi Listesi:");
+
+        foreach (string isim in ziyaretciler)
+        {
+            Console.WriteLine(isim);
+        }
+    }
+}
